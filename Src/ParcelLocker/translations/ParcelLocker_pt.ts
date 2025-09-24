b76673c597c7e39d1edc3ef677b8166f195a43ef
Seyed Sajad Hosseinirostami <S.Hosseinirostami@halm.de>
2025-09-24 16:26:53 +0200

    translation file added

---
+++ Src/ParcelLocker/translations/ParcelLocker_pt.ts
@@ -0,0 +1,147 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_PT">
+<context>
+    <name>CodeEntry</name>
+    <message>
+        <location filename="../CodeEntry.qml" line="40"/>
+        <source>Please enter the pin number.</source>
+        <translation>pt_Please enter the pin number.</translation>
+    </message>
+    <message>
+        <location filename="../CodeEntry.qml" line="143"/>
+        <source>Back</source>
+        <translation>pt_Back</translation>
+    </message>
+    <message>
+        <location filename="../CodeEntry.qml" line="150"/>
+        <source>Confirm</source>
+        <translation>pt_Confirm</translation>
+    </message>
+    <message>
+        <location filename="../CodeEntry.qml" line="162"/>
+        <source>Connections lost, Please try later</source>
+        <translation>pt_Connections lost, Please try later</translation>
+    </message>
+    <message>
+        <location filename="../CodeEntry.qml" line="173"/>
+        <source>Pin code must has 6 digits</source>
+        <translation>pt_Pin code must has 6 digits</translation>
+    </message>
+</context>
+<context>
+    <name>DoorControl</name>
+    <message>
+        <location filename="../DoorControl.qml" line="50"/>
+        <source>Rejected, Please try again</source>
+        <translation>pt_Rejected, Please try again</translation>
+    </message>
+    <message>
+        <location filename="../DoorControl.qml" line="85"/>
+        <source>Timeout, Please try again</source>
+        <translation>pt_Timeout, Please try again</translation>
+    </message>
+    <message>
+        <location filename="../DoorControl.qml" line="98"/>
+        <source>is open</source>
+        <translation>pt_is open</translation>
+    </message>
+    <message>
+        <location filename="../DoorControl.qml" line="98"/>
+        <source>is closed</source>
+        <translation>pt_is closed</translation>
+    </message>
+    <message>
+        <location filename="../DoorControl.qml" line="109"/>
+        <source>Close</source>
+        <translation>pt_Close</translation>
+    </message>
+    <message>
+        <location filename="../DoorControl.qml" line="109"/>
+        <source>Open</source>
+        <translation>pt_Open</translation>
+    </message>
+    <message>
+        <location filename="../DoorControl.qml" line="118"/>
+        <source>Connections lost, Please try later</source>
+        <translation>pt_Connections lost, Please try later</translation>
+    </message>
+    <message>
+        <location filename="../DoorControl.qml" line="132"/>
+        <source>Home</source>
+        <translation>pt_Home</translation>
+    </message>
+</context>
+<context>
+    <name>Header</name>
+    <message>
+        <location filename="../Header.qml" line="39"/>
+        <source>Light Mode</source>
+        <translation>pt_Light Mode</translation>
+    </message>
+    <message>
+        <location filename="../Header.qml" line="39"/>
+        <source>Dark Mode</source>
+        <translation>pt_Dark Mode</translation>
+    </message>
+</context>
+<context>
+    <name>Result</name>
+    <message>
+        <location filename="../Result.qml" line="39"/>
+        <source> is </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../Result.qml" line="39"/>
+        <source>open</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../Result.qml" line="39"/>
+        <source>close</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../Result.qml" line="45"/>
+        <source>Home</source>
+        <translation>pt_Home</translation>
+    </message>
+</context>
+<context>
+    <name>Welcome</name>
+    <message>
+        <location filename="../Welcome.qml" line="21"/>
+        <source>Welcome</source>
+        <translation>Pt_Welcome</translation>
+    </message>
+    <message>
+        <location filename="../Welcome.qml" line="26"/>
+        <source>Start</source>
+        <translation>pt_Start</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.cpp" line="14"/>
+        <source>Run the application in fullscreen mode.</source>
+        <translation>pt_Run the application in fullscreen mode.</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="10"/>
+        <source>Parcel Locker</source>
+        <translation>pt_Parcel Locker</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="84"/>
+        <source>Invalid Pin</source>
+        <translation>pt_Invalid Pin</translation>
+    </message>
+    <message>
+        <location filename="../main.qml" line="98"/>
+        <source>Please try again</source>
+        <translation>pt_Please try again</translation>
+    </message>
+</context>
+</TS>
--- cmake/Component.cmake
+++ cmake/Component.cmake
@@ -1,24 +1,24 @@
 include_guard(DIRECTORY)
-function(ComponentGenerator COMPONENT LIBS SOURCES_FILES)
-    target_sources(${COMPONENT} PRIVATE ${SOURCES_FILES})
-    target_link_libraries(${COMPONENT} PUBLIC ${LIBS})
-endfunction()
 
 function(DiscoverSourceFiles SRC_DIR HDR_DIR FULL_LIST)
     file(GLOB_RECURSE HEADERS   ${HDR_DIR}/*.h)
     file(GLOB_RECURSE SOURCES   ${SRC_DIR}/*.cpp)
     file(GLOB_RECURSE UIS       ${SRC_DIR}/*.ui)
     file(GLOB_RECURSE QT_RESOURCES ${SRC_DIR}/*.qrc)
+    file(GLOB_RECURSE QMLS      ${SRC_DIR}/*.qml)
 
     list(APPEND SOURCES "${HEADERS}")
     list(APPEND SOURCES "${UIS}")
     list(APPEND SOURCES "${QT_RESOURCES}")
+    list(APPEND SOURCES "${QMLS}")
     set(FULL_LIST "${SOURCES}" PARENT_SCOPE)
 endfunction()
 
 function(AutoComponentGenerator COMPONENT LIBS SRC_DIR HDR_DIR)
     DiscoverSourceFiles(${SRC_DIR} ${HDR_DIR} FULL_LIST)
-    ComponentGenerator(${COMPONENT} "${LIBS}" "${FULL_LIST}")
+    target_sources(${COMPONENT} PRIVATE ${FULL_LIST})
+    target_link_libraries(${COMPONENT} PUBLIC ${LIBS})
+
 endfunction()
 
 function(TranslationGenerator COMPONENT LANGUAGES ADDITIONAL_SOURCE COPY_INTO_SOURCE)
@@ -30,16 +30,16 @@ function(TranslationGenerator COMPONENT LANGUAGES ADDITIONAL_SOURCE COPY_INTO_SO
         list(APPEND QM "${TRANSLATION_DIR}/${COMPONENT}_${ELEMENT}.qm")
     endforeach()
 
-    find_package(Qt${Qt_MAJOR_VERSION} REQUIRED COMPONENTS LinguistTools)
+    find_package(Qt6 REQUIRED COMPONENTS LinguistTools)
     qt6_add_translations(${COMPONENT} TS_FILES ${TS}
         QM_FILES_OUTPUT_VARIABLE qm_files
         SOURCES "${ADDITIONAL_SOURCE}")
 
-    if(COPY_INTO_SOURCE)
-        add_custom_command(TARGET ${COMPONENT}_lrelease
-            POST_BUILD
-            COMMAND ${CMAKE_COMMAND} -E copy ${qm_files} ${TRANSLATION_DIR}
-            COMMENT "Move generate QM files to translation directory"
-            VERBATIM)
-    endif()
+    # if(COPY_INTO_SOURCE)
+    #     add_custom_command(TARGET ${COMPONENT}_lrelease
+    #         POST_BUILD
+    #         COMMAND ${CMAKE_COMMAND} -E copy ${qm_files} ${TRANSLATION_DIR}
+    #         COMMENT "Move generate QM files to translation directory"
+    #         VERBATIM)
+    # endif()
 endfunction()
